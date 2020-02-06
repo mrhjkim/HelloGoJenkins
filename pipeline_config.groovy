b2114d02ad7bac8acdd140a408c531933f0fbdd0
Kim HaeJoo <mrhjkim@uangel.com>
2020-02-06 11:46:30 +0900

    update config

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,5 +1,18 @@
 allow_scm_jenkinsfile = false
 libraries{
-  make
-  sonarqube
+  sdp{
+    images{
+      registry = "http://0.0.0.0:5000" // registry url
+      cred = "sdp-docker-registry"// jenkins cred id to authenticate
+      docker_args = "--network=try-it-out_sdp"  // docker runtime args
+    }
+  }
+  github_enterprise
+  sonarqube{
+    enforce_quality_gate = true
+  }
+  docker{
+    registry = "0.0.0.0:5000"
+    cred = "sdp-docker-registry"
+  }
 }
